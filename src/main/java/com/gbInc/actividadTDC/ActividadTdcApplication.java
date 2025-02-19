277db48e76afabbab376069c42b254a7de6bd595
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-02-19 18:10:09 -0300

    primer commit

---
+++ src/main/java/com/gbInc/actividadTDC/ActividadTdcApplication.java
@@ -0,0 +1,13 @@
+package com.gbInc.actividadTDC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadTdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadTdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbInc/actividadTDC/ActividadTdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbInc.actividadTDC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadTdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
