2872d0da944f7a734615efa8972dc5276a7f9c5b
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-02-20 00:43:43 -0300

    creacion de paquetes y clases

---
+++ src/main/java/com/gbInc/actividadTDC/model/Curso.java
@@ -0,0 +1,13 @@
+package com.gbInc.actividadTDC.model;
+
+import java.time.LocalDate;
+
+public class Curso {
+	
+	private Long idCurso;
+	private String nombre;
+	private String modalidad;
+	private LocalDate fechaFinalizacion;
+	private List<temas>
+	
+}
