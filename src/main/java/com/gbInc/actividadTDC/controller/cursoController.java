a3d4e83b2176e8174fe357063015707cd7654f34
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-02-21 13:22:27 -0300

    doc: se agrego la dependencia swagger para documentar los endpoints

--- src/main/java/com/gbInc/actividadTDC/controller/cursoController.java
+++ src/main/java/com/gbInc/actividadTDC/controller/cursoController.java
@@ -7,16 +7,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping("/curso")
 public class cursoController {
 
